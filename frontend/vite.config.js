8e9211f80194ee62a55be9540f2e2877b77564ef
Sheldin <lausheldin@gmail.com>
2025-07-24 00:55:39 +0000

    updated allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,9 +8,9 @@ export default defineConfig({
     host: true,
     port: 5173, // Port used by docker container
     strictPort: true,
-     watch: {
-       usePolling: true
-     }
-  }
- })
-
+    watch: {
+      usePolling: true,
+    },
+    allowedHosts: ["localhost", "cafes.caffenate.xyz"],
+  },
+});
